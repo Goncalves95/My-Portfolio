5b8388c23c4f7aa03f89b9a623a7e097292e354f
Fernando Gonçalves <fernando_jcg22@hotmail.com>
2024-08-21 14:31:24 +0000

    Add a automatation to copyrights-year

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 AOS.init({
-    duration: 1000,
-  })
+  duration: 1000,
+})
+
+const copyrightYear = document.getElementById("copyright-year");
+copyrightYear.innerHTML = `&copy; ${new Date().getFullYear()} Fernando Gonçalves`;
